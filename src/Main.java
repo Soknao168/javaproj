031b0cf29e01fd6d61e143e94f39c14b9e08482d
Fixhe <ravinmengheang830@gmail.com>
2024-02-08 18:56:37 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
-    //test branch
+
+    //branch Ravin
     public static void main(String[] args) {
 //        RoomInfo[] B1= new RoomInfo[10];//{new RoomInfo("R001","AAA","VIP",5,25,"Free")};
 //        for (int i=0; i<2; i++) {
